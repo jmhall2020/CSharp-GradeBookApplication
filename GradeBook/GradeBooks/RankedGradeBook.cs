2099ae0b60dda8dd109f4503dd9bdd3ee9bbe5a8
Jeremy Hall <jeremy.m.hall@emcins.com>
2020-03-18 10:30:02 -0500

    GetLetterGrade override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,15 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+         
+            return 'F';
+        }
     }
 }
