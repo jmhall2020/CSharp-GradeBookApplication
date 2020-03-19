86f4f048885d4835a49ea7b451f0ae4519a4fba9
Jeremy Hall <jeremy.m.hall@emcins.com>
2020-03-19 08:19:58 -0500

    yay

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,7 +17,34 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException();
             }
-         
+
+            //here
+            //create an array of average scores by iterating through Students, 
+            //the List of student objects.
+
+            double[] avgGrades = new double[Students.Count];
+
+            for  (int i = 0; i <= Students.Count; i++)
+            {
+                avgGrades[i] = Students[i].AverageGrade;
+            }
+
+            Array.Sort(avgGrades);
+            Array.Reverse(avgGrades, 0, Students.Count);
+            var x = (Students.Count / 5) - 1;    //needed to account for zero index?  OK?????
+
+
+            if (averageGrade >= avgGrades[x])
+            {
+                return 'A';
+            }
+
+
+            //x = top 20% = A
+            //x * 2 = top 40% = B
+            //x * 3 = top 60% = C
+
+
             return 'F';
         }
     }
